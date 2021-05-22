d45d3f08e2b71b4b695c9de9996ed04bdd0acdcc
Kaushal Kumar Singh <kaushal.singh@redbus.com>
2021-05-23 01:47:52 +0530

    created budget manager

---
+++ eventPage.js
@@ -0,0 +1,35 @@
+var contextMenuItem = {
+    "id": "spendMoney",
+    "title": "Spend Money",
+    "contexts" : ["selection"]
+};
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if(clickData.menuItemId == "spendMoney" && clickData.selectionText){
+        if(typeof parseInt(clickData.selectionText) == "number"){
+            chrome.storage.sync.get(["total", "limit"], function(budget){
+                var newTotal = 0;
+                if(budget.total){
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(clickData.selectionText.replace(",", ""));
+                chrome.storage.sync.set({"total": newTotal}, function(){
+                    if(newTotal >= budget.limit){
+                        var notifOptions = {
+                            type : "basic",
+                            iconUrl: "./am_14.svg",
+                            title : "limit reached",
+                            message : "oops! looks like u reached ur limit"
+                        }
+                        chrome.notifications.create("limitNotif", notifOptions);
+                    }
+                })
+            })  
+        }
+    }
+})
+
+chrome.storage.onChanged.addListener(function(changes, storageName){
+    chrome.browserAction.setBadgeText({"text":changes.total.newValue.toString()});
+})
---
+++ popup.js
@@ -0,0 +1,36 @@
+$(function(){
+    chrome.storage.sync.get(["total", "limit"], function(budget){
+        if(budget.total){
+            $("#total").text(budget.total);
+        }
+        else{
+            $("#total").text(0);
+        }
+        $("#limit").text(budget.limit);
+    })
+    $("#spendAmount").click(function(){
+        chrome.storage.sync.get(["total", "limit"], function(budget){
+            var newTotal = 0;
+            if(budget.total){
+                newTotal += parseInt(budget.total);
+            }
+            var amount = $("#amount").val();
+            if(amount){
+                newTotal+=parseInt(amount);
+            }
+            chrome.storage.sync.set({"total": newTotal}, function(){
+                if(amount && newTotal >= budget.limit){
+                    var notifOptions = {
+                        type : "basic",
+                        iconUrl: "./am_14.svg",
+                        title : "limit reached",
+                        message : "oops! looks like u reached ur limit"
+                    }
+                    chrome.notifications.create("limitNotif", notifOptions);
+                }
+            });
+            $("#total").text(newTotal);
+            $("#amount").val("");
+        });
+    })
+})
